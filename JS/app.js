e9f022ba6941622ac114a0ad8405303c876e3dbe
typout31 <nicholas.chu98@gmail.com>
2021-03-26 17:22:58 -0700

    javascript coded

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,21 @@
-// your JavaScript goes here, you can delete this comment
+var todoList =  [
+    {todo: "clone repo for starter code", status:"completed"}, 
+    {todo: "link stylesheet and JavaScript", status:"completed"},
+    {todo: "check if todoList is complete", status:"started"},
+    {todo: "push code to own GitHub repo", status:"started"}
+];
+
+let completedTodos = [];
+
+for (i = 0; i < todoList.length; i++) {
+    if(todoList[i].status === "completed") {
+        completedTodos.push(todoList[i])
+    }
+}
+  
+for (i = 0; i < completedTodos.length; i++) {
+    console.log(completedTodos[todo])
+}
+
+
+
